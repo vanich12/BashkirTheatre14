f0c59c04a7f4b14dc6adabc138cd39b5a0830341
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-16 11:48:46 +0400

    perf: оптимизация

--- BashkirTheatre14/Helpers/Logging/FileLoggingService.cs
+++ BashkirTheatre14/Helpers/Logging/FileLoggingService.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿ using System.IO;
 using System.Text;
 
 namespace BashkirTheatre14.Helpers.Logging
---
+++ BashkirTheatre14/Services/PreloadHttpRequestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BashkirTheatre14.Model;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BashkirTheatre14.Services
+{
+    public  class PreloadHttpRequestData
+    {
+        private readonly IMainApiClient _apiClient;
+        private readonly IMemoryCache _cache;
+
+        public PreloadHttpRequestData(IMainApiClient apiClient, IMemoryCache cache)
+        {
+            _apiClient = apiClient;
+            _cache = cache;
+        }
+
+        //public async Task<IEnumerable<>> GetChroniclesDataAsync()
+        //{
+
+        //}
+    }
+}
