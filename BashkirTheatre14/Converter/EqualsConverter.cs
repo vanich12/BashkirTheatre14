34c2a638579505a69e248f7e87013935ec8351a4
muxastepan <58739030+muxastepan@users.noreply.github.com>
2024-08-23 12:20:57 +0400

    feat: убран функционал зума с карты

---
+++ BashkirTheatre14/Converter/EqualsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BashkirTheatre14.Converter
+{
+    public class EqualsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return null;
+
+            if (values[0] is null)
+                return values[1] is null;
+
+            return values[0].Equals(values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
